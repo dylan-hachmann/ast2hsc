7a6b2303d7f69dad9be022373f87b6331ab3bbf0
Dylan Hachmann <dylanhachmann@gmail.com>
2024-10-10 03:23:54 -0700

    Add header to test typedefs

---
+++ test/typedefs.h
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+// Nested typedefs
+typedef int inte;
+typedef inte integ;
+typedef integ intege;
+typedef intege integer;
+
+integer add4(intege, integ, inte, int);
+
+// Struct
+typedef struct {
+  int a;
+  long b;
+  float c;
+} some_nums;
+
+bool crunch(some_nums);
